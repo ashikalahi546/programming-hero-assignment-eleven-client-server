9f1367137e73c0c35f856a964f42e36ea99de3f7
ashikalahi546 <144088399+ashikalahi546@users.noreply.github.com>
2024-05-18 14:54:25 +0600

    added axios

--- src/pages/Assignments.jsx
+++ src/pages/Assignments.jsx
@@ -1,16 +1,22 @@
 import React, { useContext, useEffect, useState } from 'react';
 import Assignment from './Assignment';
+import axios from 'axios';
 const Assignments = () => {
     const [allData,setData] = useState([])
 
     useEffect(()=>{
-        fetchData()
+        (async()=>{
+            const res = await axios.get(`${import.meta.env.VITE_APP_URL}/create`)
+            const data = res.data
+            setData(data)
+        })()
+        // fetchData()
     },[])
-    const fetchData =()=>{
-        fetch(`${import.meta.env.VITE_APP_URL}/create`)
-        .then(res=>res.json())
-        .then(data=> setData(data))
-    }
+    // const fetchData =()=>{
+    //     fetch(`${import.meta.env.VITE_APP_URL}/create`)
+    //     .then(res=>res.json())
+    //     .then(data=> setData(data))
+    // }
 
     return (
         <div className='grid xl:grid-cols-2  xl:w-9/12 lg:w-10/12 mx-auto lg:px-0 px-5 gap-5'>
